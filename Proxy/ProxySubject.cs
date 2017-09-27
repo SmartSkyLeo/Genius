501b8f024cee731a0f66948477433e46c50f2584
SKLiu <SKLiu@NLTC566>
2017-09-27 15:25:51 +0800

    Add project files.

---
+++ Proxy/ProxySubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class ProxySubject : Subject
+    {
+        RealSubject realsubject = new RealSubject();
+        public override void Request()
+        {
+            if (realsubject == null) {
+                realsubject = new RealSubject();
+            }
+            realsubject.Request();
+        }
+    }
+}
